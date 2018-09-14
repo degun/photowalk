d9b6794b5dd195fb063a0e361f03bb1c165b4ed3
degun <pretty.lonely.place@hotmail.com>
2018-09-14 19:59:17 +0200

    deploy

--- src/pjeset/Item.js
+++ src/pjeset/Item.js
@@ -52,7 +52,7 @@ class Item extends Component {
 	render() {
 		return (
 		<div onMouseEnter={this.mouseEntered} onMouseLeave={this.mouseLeft} className="item" >
-			<Link className="photolink" to={!this.props.back ? ('/'+this.props.title.toLowerCase()+'/:'+ this.props.id) : '#'}><img allowFullScreen={true} src={this.props.src} alt={this.props.name} onClick={this.goFullscreen}/></Link>
+			<Link className="photolink" to={!this.props.back ? ('/'+this.props.title.toLowerCase()+'/:'+ this.props.id) : '#'}><img allowFullScreen src={this.props.src} alt={this.props.name} onClick={this.goFullscreen}/></Link>
 			<div style={{display: this.props.details ? this.state.display : 'none', bottom: (this.isMobileDevice() || this.state.hovered) ? '49px' : '-100px'}}className="description">
 				<h4 className="name">{this.props.name}</h4>
 				<div className="placedate">
